74aade00f5e5154e5624e62e9eed950fdce516df
michael-large <michael.large@cdinteractive.com>
2017-07-04 20:14:44 -0400

    Retro style overhaul

--- scripts/retro-app.js
+++ scripts/retro-app.js
@@ -7,6 +7,12 @@ retroApp.factory('mySocket', function(socketFactory){
 
 retroApp.controller('RetroListController', function ($scope, mySocket, $timeout){
 
+  $scope.autoExpand = function(e) {
+      var element = typeof e === 'object' ? e.target : document.getElementById(e);
+  		var scrollHeight = element.scrollHeight - 10; // replace 60 by the sum of padding-top and padding-bottom
+      element.style.height =  scrollHeight + "px";
+  };
+
   $scope.save = function(index, content, element){
     console.log("Save Card: " + content);
     element.isEdit = false;
